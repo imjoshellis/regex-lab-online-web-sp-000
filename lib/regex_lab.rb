32cd9b7bfd74d6b799028256e5a706d25eb3d030
Josh Ellis <joshe523@gmail.com>
2020-03-02 19:08:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,5 +20,5 @@ valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 invalid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
 
 def valid_phone_number?(phone)
-  if phone.match(\(?\d{3}\)?\s?\d{3}(-?|\s?)\d{4}) ? true : false
+  if phone.match(/\(?\d{3}\)?\s?\d{3}(-?|\s?)\d{4}/) ? true : false
 end
